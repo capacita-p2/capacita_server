99c3c6d7495deaa8243a812a0fc2f3c2f5562ac8
Josemilson Júnior <josemilson.junior@a.ficr.edu.br>
2020-11-04 21:57:48 -0300

    Relacionamento Many to Many Curso e Tipo de deficiência

--- src/models/curso.js
+++ src/models/curso.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Curso.belongsTo(models.Instituicao)
       Curso.belongsToMany(models.Usuario_pcd, { through: models.Inscricao })
-      Curso.belongsToMany(models.Tipo_deficiencia, { through: models.curso_tipo_deficiencia })
+      Curso.belongsToMany(models.Tipo_deficiencia, { through: models.Curso_tipo_deficiencia })
       Curso.hasMany(models.Avaliacao)
     }
   };
