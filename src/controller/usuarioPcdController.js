fcebafd87c5f9a7bf0c2c96a3d847b8bcc56f84d
Josemilson Júnior <josemilson.junior@a.ficr.edu.br>
2020-11-10 21:42:24 -0300

    Correção no controlador de usuariosPcd para tab usuario_deficiência

--- src/controller/usuarioPcdController.js
+++ src/controller/usuarioPcdController.js
@@ -1,5 +1,6 @@
 const UsuarioPcd = require('../models').Usuario_pcd
 const Usuario = require('../models').Usuario
+const UsuarioDeficiencia = require('../models').Usuario_tipo_deficiencia
 
 exports.listAll = (req, res) => {
     UsuarioPcd.findAll ().then(usuariosPcd => {
@@ -9,18 +10,35 @@ exports.listAll = (req, res) => {
     })
 }
 
-exports.createOne = (req, res) => {
-    let {nome, telefone, endereco, numero, bairro, cidade, id_estado, id_usuario, cep, cpf, ativo, email, senha, tipo} = req.body
+exports.createOne = async (req, res) => {
+    let {nome, telefone, endereco, numero, bairro, cidade, id_estado, id_usuario, cep, cpf, ativo, email, senha, tipo, deficiencias} = req.body
     Usuario.create({email, senha, ativo, tipo})
-    .then(usuario => {
+    .then(async usuario => {
         id_usuario = usuario.id
         UsuarioPcd.create({nome, telefone, endereco, numero, bairro, cidade, id_estado, id_usuario, cep, cpf, ativo})
-        .then (usuarioPcd => {
-            res.send(usuario.Usuario)
-            res.send(usuario.UsuarioPcd)
+        .then (async usuarioPcd => {
+            var tiposDeficiencias = []
+
+            for(deficiencia in deficiencias) {
+                var dado = {
+                    id_usuario_pcd: usuarioPcd.id,
+                    id_tipo_deficiencia: Number.parseInt(deficiencia)+1
+                }
+                tiposDeficiencias.push(dado)
+            }
+
+            console.log(tiposDeficiencias)
+
+            UsuarioDeficiencia.bulkCreate(tiposDeficiencias).then(usuarioDeficiencias => {
+                usuarioPcd.tiposDeficiencias = usuarioDeficiencias
+            }).catch(err => {
+                res.send(err)
+            })
+            usuario.usuarioPcd = usuarioPcd
         }).catch(err => {
             res.send(err)
         })
+        res.send(usuario)
     }).catch(err => {
         res.send(err)
     })
